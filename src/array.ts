c6229e0a923ff00dc8dc156512e40c5dd40560bd
time-shimizu <213.f.r48132@gmail.com>
2021-02-16 23:22:42 +0900

    add array.ts

---
+++ src/array.ts
@@ -0,0 +1,17 @@
+export {};
+
+let numbers: number[] = [1,2,3];
+
+let numbers2: Array<number> = [1,2,3];
+
+let strings2: Array<string> = ['東京','大阪','京都'];
+
+let strings: string[] = ['TypeScript', 'JabaScript', 'CoffeeScript'];
+
+let nijigenhairetu: number[][] = [
+  [1,2],
+  [2,3],
+  [3,4]
+];
+
+let hairetu: (string|number|boolean)[] = [1,false,'Japan'];
