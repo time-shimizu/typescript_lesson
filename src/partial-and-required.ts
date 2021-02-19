4809842f34d9081b464f1d5da760500126db20ed
time-shimizu <213.f.r48132@gmail.com>
2021-02-19 14:48:07 +0900

    add partial-and-required.ts

---
+++ src/partial-and-required.ts
@@ -0,0 +1,16 @@
+export {};
+
+type Profile = {
+  name: string;
+  age?: number;
+  zipCode: number;
+};
+
+// type Profile2 = {
+//   name?: string;
+//   age?: number;
+// };
+
+type PartialType = Partial<Profile>;
+
+type RequiredType = Required<Profile>;
